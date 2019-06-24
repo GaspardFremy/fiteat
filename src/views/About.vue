db4f55edeb1f823b8ba5cde6edbe29ef891e214b
GaspardFremy <gaspardfremy123@gmail.com>
2019-06-24 17:18:47 +0200

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <h4>Exemple</h4>
+        <div>
+            <div v-for="data in restaurants">{{data[0].name}}</div>
+            <div v-if="seen">TOTO</div>
+        </div>
+    </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import json from '@/assets/data_restaurants.json'
+
+export default {
+    name: 'home',
+    components: {
+        HelloWorld
+    },
+    data(){
+        return{
+            restaurants: json,
+            seen: true
+        }
+    },
+    comuted: {
+
+    }
+}
+</script>
